0ec4d1ef310d981fc48699dac1af4ef8f353bc96
Cuycr77 <143004840+Cuycr77@users.noreply.github.com>
2025-04-16 10:35:37 -0500

    Primer commit del backend FastAPI con Docker y MySQL

--- conexion.py
+++ conexion.py
@@ -2,8 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-URL_DB="mysql+mysqlconnector://cristian:0000@localhost:3307/proyecto_sena"
+#URL_DB="mysql+mysqlconnector://cristian:0000@localhost:3307/proyecto_sena"
 #URL_DB="mysql+mysqlconnector://root:0000@localhost:3306/proyecto_sena"
+URL_DB="mysql+mysqlconnector://root:phuYvJhwoDQBhJRnHpJxRJcbFsVMEGKg@mysql.railway.internal:3306/proyecto_sena"
 
 crear=create_engine(URL_DB)
 Sessionlocal=sessionmaker(autocommit=False, autoflush=False, bind=crear)
